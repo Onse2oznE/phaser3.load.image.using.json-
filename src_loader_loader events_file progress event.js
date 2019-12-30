b05de73fc77ef9d58a8bf62ff15242c27c014c96
OzonE24 <43329768+Onse2oznE@users.noreply.github.com>
2019-12-30 18:43:51 +0800

    Add files via upload

---
+++ src_loader_loader events_file progress event.js
@@ -0,0 +1,51 @@
+﻿var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'phaser-example',
+    pixelArt: true,
+    scene: {
+        preload: preload,
+        create: create,
+        pack: {
+            files: [
+                { type: 'image', key: 'bear', url: 'assets/pics/alex-bear.png' }
+            ]
+        }
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.add.image(400, 300, 'bear').setScale(2.5);
+
+    var progress = this.add.graphics();
+
+    this.load.on('fileprogress', function (file, value) {
+
+        if (file.key === 'goldrunner')
+        {
+            progress.clear();
+            progress.fillStyle(0xffffff, .9);
+            progress.fillRect(120 - (value * -0), 400 , value*550,100);
+        }
+
+    });
+
+    this.load.on('complete', function () {
+
+        progress.destroy();
+
+    });
+
+    this.load.audio('goldrunner', 'assets/audio/Scyphe-Goldrunner_(Maccie_Pimp_Me Up_Remix).mp3');
+}
+
+function create ()
+{
+    var music = this.sound.add('goldrunner');
+
+    music.play();
+}
---
+++ src_loader_loader events_load progress.js
@@ -0,0 +1,59 @@
+﻿var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    var progress = this.add.graphics();
+
+    this.load.on('progress', function (value) {
+
+        progress.clear();
+        progress.fillStyle(0xffffff, 1);
+        progress.fillRect(20, 270, 720 * value, 60);
+
+    });
+
+    this.load.on('complete', function () {
+
+        progress.destroy();
+
+    });
+
+    //  Now let's load a huge stack of files!
+
+    this.load.setPath('assets/sprites/');
+
+    this.load.image('128x128');
+    this.load.image('128x128-v2');
+    this.load.image('a');
+    this.load.image('advanced_wars_land');
+    this.load.image('advanced_wars_tank');
+    this.load.image('amiga-cursor');
+    this.load.image('aqua_ball');
+    this.load.image('arrow');
+    this.load.image('arrows');
+    this.load.image('asteroids_ship');
+}
+
+function create ()
+{
+    var keys = this.textures.getTextureKeys();
+
+    for (var i = 0; i < keys.length; i++)
+    {
+        var x = Phaser.Math.Between(50,700);
+        var y = Phaser.Math.Between(50, 500);
+
+        this.add.image(x, y, keys[i]);
+    }
+}
